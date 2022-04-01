3417dbc0d5ef904a458956aee1535d312a5b5da7
Abdessamad el akroudi <lsroudi@gmail.com>
2022-04-01 19:13:32 +0200

    add mutex

--- src/lib.rs
+++ src/lib.rs
@@ -114,3 +114,23 @@ pub fn mutex_test_single_th(){
 
     println!("m = {:?}", m);
 }
+
+use std::sync::Arc;
+pub fn mutex_test_multiple_th(){
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter =Arc::clone(&counter);
+        let handle = thread::spawn(move ||{
+            let mut num = counter.lock().unwrap();
+            *num+=1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+         handle.join().unwrap();
+       }
+       println!("Result: {}", *counter.lock().unwrap());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 
 
 fn main() {
-    //rust_concurrency::thread_test();
-    //rust_concurrency::thread_test_fix();
-    //rust_concurrency::thread_test_with_value();
-    //rust_concurrency::channel_test();
-    //rust_concurrency::channel_test_with_multiple_producer();
+    rust_concurrency::thread_test();
+    rust_concurrency::thread_test_fix();
+    rust_concurrency::thread_test_with_value();
+    rust_concurrency::channel_test();
+    rust_concurrency::channel_test_with_multiple_producer();
     rust_concurrency::mutex_test_single_th();
+    rust_concurrency::mutex_test_multiple_th();
 
 }
